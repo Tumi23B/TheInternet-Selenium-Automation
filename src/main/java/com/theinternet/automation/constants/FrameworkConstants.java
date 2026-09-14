0787e8a136306863124ede347f2bc738b5d96f00
Boitumelo Khauoe <BoitumeloKhauoe@africanresonance.com>
2026-09-14 21:33:38 +0200

    Selenium project setup

---
+++ src/main/java/com/theinternet/automation/constants/FrameworkConstants.java
@@ -0,0 +1,26 @@
+package com.theinternet.automation.constants;
+
+public final class FrameworkConstants {
+
+    private FrameworkConstants() {
+        // Prevent instantiation
+    }
+
+    public static final String CONFIG_FILE_PATH =
+            "configuration/config.properties";
+
+    public static final String TEST_DATA_FILE_PATH =
+            "configuration/testdata.properties";
+
+    public static final String SCREENSHOTS_DIRECTORY =
+            "screenshots";
+
+    public static final String REPORTS_DIRECTORY =
+            "reports";
+
+    public static final String TEST_DATA_DIRECTORY =
+            "test-data";
+
+    public static final String UPLOAD_FILE_NAME =
+            "upload-test.txt";
+}
